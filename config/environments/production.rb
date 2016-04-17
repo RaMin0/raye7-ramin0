8623cfc3b8129c588dee8e0c066a680d2e5facb3
Ramy Aboul Naga <ramy.naga@gmail.com>
2016-04-17 14:21:34 +0200

    Fix assets host url to follow used protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://#{ENV['HOST_URL']}"
+  config.action_controller.asset_host = "//#{ENV['HOST_URL']}"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
