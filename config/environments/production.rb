c0ad0123a072778d656b9f31f6bc95c88f397543
Ramy Aboul Naga <ramy.naga@gmail.com>
2016-04-17 13:29:16 +0200

    Debugging production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = Rails.application.secrets.action_mailer
+  config.action_mailer.smtp_settings = Rails.application.secrets.action_mailer.symbolize_keys
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
