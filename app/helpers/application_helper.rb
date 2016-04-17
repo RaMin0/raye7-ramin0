e83e9b388ef74995cec23e59ee690ce235c10bc4
Ramy Aboul Naga <ramy.naga@gmail.com>
2016-04-17 06:08:44 +0200

    Basic devise configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def flash_messages
+    %w(notice alert).map do |flash_message_type|
+      flash_message_type = flash_message_type.to_sym
+      flash_message      = flash[flash_message_type]
+      flash.delete(flash_message_type)
+
+      %Q{
+        <div class="alert alert-#{flash_message_type_css_class(flash_message_type)}">
+          <button type="button" class="close" data-close="alert"></button>
+          <span>#{flash_message}</span>
+        </div>
+      } if flash_message
+    end.join.html_safe
+  end
+
+  def flash_message_type_css_class(flash_message_type)
+    case flash_message_type
+    when :notice then 'success'
+    when :alert  then 'danger'
+    end
+  end
+  
+  def error_messages(f)
+    f.error_messages header_message: nil, class: 'alert alert-danger'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
 Rails.application.routes.draw do
+  # Web
+  devise_for :users,
+    path: '',
+    path_names: { sign_in: 'login', sign_out: 'logout', password: 'forgot_password' },
+    only: [:sessions, :passwords, :confirmations]
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :new_user_registration
+    post 'register', to: 'devise/registrations#create'
+    get 'profile/edit', to: 'devise/registrations#edit', as: :edit_user_registration
+    patch 'profile', to: 'devise/registrations#update', as: :user_registration
+    delete 'profile', to: 'devise/registrations#destroy'
+  end
+  
+  root to: 'application#index'
 end
